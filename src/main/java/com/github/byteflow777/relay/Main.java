8e01d174bcdf57fd10261174ff4e90d50945bb42
Leon Li <byteflow777@gmail.com>
2022-05-09 14:42:54 +0800

    init

---
+++ src/main/java/com/github/byteflow777/relay/Main.java
@@ -0,0 +1,15 @@
+package com.github.byteflow777.relay;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    final static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.error("Hello World");
+    }
+}
